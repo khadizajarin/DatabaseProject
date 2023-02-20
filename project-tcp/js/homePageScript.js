d04912e817a4a7f229c42befe027afbfedcf325f
Saima-Amin <121403498+Saima-Amin@users.noreply.github.com>
2023-02-21 05:02:46 +0600

    Add files via upload

--- project-tcp/js/homePageScript.js
+++ project-tcp/js/homePageScript.js
@@ -2,8 +2,9 @@ let  searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
      searchForm.classList.toggle('active');
+     sellerProfile.classList.remove('active');
      navbar.classList.remove('active');
-    cartItem.classList.remove('active');
+     cartItem.classList.remove('active');
 }
 
 
@@ -11,6 +12,16 @@ let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () => {
     cartItem.classList.toggle('active');
+    sellerProfile.classList.remove('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let sellerProfile = document.querySelector('.seller-profile-container');
+
+document.querySelector('#user-btn').onclick = () => {
+    sellerProfile.classList.toggle('active');
+    cartItem.classList.remove('active');
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
 }
@@ -19,4 +30,21 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
+    sellerProfile.classList.remove('active');
 }
+
+
+function displayImage(file){
+    const img = document.querySelector(".js-image");
+    img.src = URL.createObjectURL(file);
+}
+
+
+document.getElementById('add-to-homapage').addEventListener('click',function(){
+    const profileImg = document.getElementById('profile-img');
+    const img = profileImg.innerText;
+    const cardImg = document.getElementById('card-img');
+    cardImg.innerText = img;
+
+
+})
